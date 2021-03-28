3713cd8a12d344d7c02a8ee83d6d9d633797c57e
313974 <313974@cs.ktu.edu.tr>
2021-03-28 15:33:27 +0300

    E-ticaret kodu yenilendi -> for, foreach ve while döngüleri ile ürün yazdırma yapıldı. Ref ve Out keywordleri kullanımı denendi. Değer ve referans tipli değişkenler ile ilgili bir örnek. switch-case kullanıldı.

---
+++ E-ticaret/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class ReplitOdev
+{
+    public static void Main(string[] args)
+    {
+        Product product1 = new Product();
+        product1.UrunAdi = "Cepli Sarı Basic Tişört";
+        product1.UrunFiyati = 129.90;
+        product1.UrunIndirimOrani = "%20";
+
+        Product product2 = new Product();
+        product2.UrunAdi = "Dar Kesim Pantolon";
+        product2.UrunFiyati = 119.90;
+        product2.UrunIndirimOrani = "%30";
+
+        Product[] products = new Product[] { product1, product2 };
+
+        Console.WriteLine("-------------------for----------------------");
+        for (int i = 0; i < products.Length; i++)
+        {
+            Console.WriteLine("Ürün Adi: " + products[i].UrunAdi +
+                "| Ürün Fiyati: " + products[i].UrunFiyati +
+                "| Ürün indirim Orani: " + products[i].UrunIndirimOrani);
+        }
+
+        Console.WriteLine("-----------------foreach--------------------");
+        foreach (Product product in products)
+        {
+            Console.WriteLine("Ürün Adi: " + product.UrunAdi +
+                "| Ürün Fiyati: " + product.UrunFiyati +
+                "| Ürün indirim Orani: " + product.UrunIndirimOrani);
+        }
+
+        Console.WriteLine("-----------------foreach--------------------");
+        int a = 0;
+        while (a < products.Length)
+        {
+            Console.WriteLine("Ürün Adi: " + products[a].UrunAdi +
+                "| Ürün Fiyati: " + products[a].UrunFiyati +
+                "| Ürün indirim Orani: " + products[a].UrunIndirimOrani);
+            a++;
+        }
+    }
+}
+
+class Product
+{
+    public string UrunAdi { get; set; }
+
+    public double UrunFiyati { get; set; }
+
+    public string UrunIndirimOrani { get; set; }
+}
--- Kamp/Program.cs
+++ Kamp/Program.cs
@@ -6,7 +6,39 @@ namespace Kamp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ternary operatorü; kısa bir if-else kullanım şeklidir
+            int num = 5;
+            string str = (num < 4) ? "true" : "false";
+            Console.WriteLine(str);
+
+            //switch-case operatörü: if-else yapısının basit bir halidir..
+            double euro = 9;
+            switch (euro)
+            {
+                case 1:
+                    Console.WriteLine("answer 1");
+                    break;
+                case 2:
+                    Console.WriteLine("answer 2");
+                    break;
+                case 9:
+                    Console.WriteLine("answer 9");
+                    break;
+                default:
+                    Console.WriteLine("unknown value!");
+                    break;
+            }
+
+            //while yapısı kullanılırken önce bir değer ataması yapılır.
+            //Sonra while içine bir şart yazılır. değişken değeri while
+            //içerisinde arttırılabilir. Her döngüde şartın sağlanıp
+            //sağlanmadığı kontrol edilir. 
+            int temp = 5;
+            while (temp > 0)
+            {
+                Console.WriteLine("sa");
+                temp -= 1;
+            }
         }
     }
 }
