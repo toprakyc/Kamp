75f3826e90dcbecdea74731cf6f947d4dd630aad
313974 <313974@cs.ktu.edu.tr>
2021-03-28 12:27:02 +0300

    3. work Customer Management

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void AddCustomer(Musteri musteri)
+        {
+            Console.WriteLine("Customer has added! Customer Information: " 
+                + musteri.Name + " " + musteri.Surname);
+        }
+
+        public void DelCustomer(Musteri musteri)
+        {
+            Console.WriteLine("Customer has deleted! Customer Information: "
+                + musteri.Name + " " + musteri.Surname);
+        }
+
+        public void ListAllCustomers(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Name + " " + musteri.Surname + " Id: " 
+                    + musteri.Id + " Credit Point: " + musteri.CreditPoint);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 102936;
+            musteri1.Name = "Mark";
+            musteri1.Surname = "Zuckerberg";
+            musteri1.CreditPoint = 9;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 213843;
+            musteri2.Name = "Elon";
+            musteri2.Surname = "Musk";
+            musteri2.CreditPoint = 8;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 787421;
+            musteri3.Name = "Faber";
+            musteri3.Surname = "Castell";
+            musteri3.CreditPoint = 5;
+
+            Musteri[] musteriler = new Musteri[] {musteri1, musteri2, musteri3};
+
+            MusteriManager musteriManager = new MusteriManager();
+            Console.WriteLine("---------------------Customer-Add---------------------------");
+            musteriManager.AddCustomer(musteri1);
+            musteriManager.AddCustomer(musteri2);
+            musteriManager.AddCustomer(musteri3);
+            Console.WriteLine("---------------------Customer-Del---------------------------");
+            musteriManager.DelCustomer(musteri1);
+            musteriManager.DelCustomer(musteri2);
+            musteriManager.DelCustomer(musteri3);
+            Console.WriteLine("---------------------Customer-List---------------------------");
+            musteriManager.ListAllCustomers(musteriler);
+        }
+    }
+}
